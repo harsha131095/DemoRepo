85d56d9825aa30a3186a92b91130fccc308f10ad
Harsha Marathe <harshamarathe11296@gmail.com>
2020-11-19 15:04:46 -0500

    Added new search page feature code

--- src/main/java/com/qa/pages/LoginPage.java
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -5,5 +5,9 @@ public class LoginPage {
 	public void loginPage() {
 		System.out.println("This is login page method");
 	}
+	
+	public void lpage() {
+		System.out.println("This is the login page method 2");
+	}
 
 }
