122897278c71a245b90c1387032bbcf76d637361
harsha131095 <72995788+harsha131095@users.noreply.github.com>
2020-11-19 15:29:06 -0500

    Update TestLibs.java

--- src/main/java/com/qa/libs/TestLibs.java
+++ src/main/java/com/qa/libs/TestLibs.java
@@ -5,5 +5,9 @@ public class TestLibs {
 	public void testLibs() {
 		System.out.println("This is test Library");
 	}
+	
+	public void testlib() {
+		System.out.println("This is test library 2")
+	}
 
 }
