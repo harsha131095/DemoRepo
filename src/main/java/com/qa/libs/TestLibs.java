617b8ff7610ca2e13ed1ceb0e912762cbd76b7c4
Harsha Marathe <harshamarathe11296@gmail.com>
2020-11-19 14:31:17 -0500

    First git commit for selenium code

---
+++ src/main/java/com/qa/libs/TestLibs.java
@@ -0,0 +1,9 @@
+package com.qa.libs;
+
+public class TestLibs {
+	
+	public void testLibs() {
+		System.out.println("This is test Library");
+	}
+
+}
---
+++ src/main/java/com/qa/util/TestUtil.java
@@ -0,0 +1,9 @@
+package com.qa.util;
+
+public class TestUtil {
+	
+	public void testUtil() {
+		System.out.println("This is test utils method");
+	}
+
+}
